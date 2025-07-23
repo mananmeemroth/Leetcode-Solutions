bbd642aa8f3eeb1665c78d80710d4656ec3db178
MANAN MEEMROTH <mananmeemroth@MANANs-MacBook-Pro-M3.local>
2025-07-24 00:01:21 +0530

    Added today's Leetcode solutions

--- Arrays/1838.cpp
+++ Arrays/1838.cpp
@@ -4,10 +4,22 @@ using namespace std;
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
-        int limit=k;
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            
+        sort(nums.begin(), nums.end());
+        int left = 0, right = 0;
+        long res = 0, total = 0;
+
+        while (right < nums.size()) {
+            total += nums[right];
+
+            while (nums[right] * static_cast<long>(right - left + 1) > total + k) {
+                total -= nums[left];
+                left += 1;
+            }
+
+            res = max(res, static_cast<long>(right - left + 1));
+            right += 1;
         }
+
+        return static_cast<int>(res);        
     }
 };
