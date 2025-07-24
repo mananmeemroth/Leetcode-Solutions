50ee186771c758b7c248f8b4b70dd3cf80196a5b
MANAN MEEMROTH <mananmeemroth@MANANs-MacBook-Pro-M3.local>
2025-07-24 23:16:11 +0530

    today's solution

---
+++ Arrays/Linked List/deleteNode.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+
+    // Alternative, we can copy next node's properties directly with
+        // *node = *node->next;
+    }
+};
