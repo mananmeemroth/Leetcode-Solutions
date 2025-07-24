e7cf9d57c7b4f606899192b9cb8090c7464a1bc4
MANAN MEEMROTH <mananmeemroth@MANANs-MacBook-Pro-M3.local>
2025-07-24 23:10:29 +0530

    today's solution

---
+++ Strings/removeoutermostparentheses.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result; 
+        int balance = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                if (balance > 0) {
+                    result += s[i];
+                }
+                balance++;
+            } else {
+                balance--; 
+                if (balance > 0) {
+                    result += s[i];
+                }
+            }
+        }
+
+        return result; 
+                      
+    }
+};
