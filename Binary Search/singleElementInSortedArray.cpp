4675507020eb20e61990d436bcfe291cbc6615d4
Manan Meemroth <divyanshmeemroth@gmail.com>
2025-08-07 18:58:52 +0530

    added some changes

--- Binary Search/singleElementInSortedArray.cpp
+++ Binary Search/singleElementInSortedArray.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
-class Solution {
+
+// solution 1
+class Solution { // with bitwise operator
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int low = 0, high = nums.size() - 2;
@@ -13,5 +15,49 @@ public:
             }
         }
         return nums[low];
+    }
+};
+// solution 2
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& A) {
+        int n=A.size();
+        
+        if(n==1) return A[0];
+        int start=0,end=n-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(mid==0&& A[0]!=A[1]) return A[mid];
+            if(mid==n-1&&A[n-1]!=A[n-2]) return A[mid];
+
+            if(A[mid-1]!=A[mid]&&A[mid]!=A[mid+1]) return A[mid];
+
+            if(mid%2==0){
+                if(A[mid-1]==A[mid]){
+                    end =mid-1;
+                }else{start=mid+1;}
+            }else{
+                if(A[mid-1]==A[mid]){
+                    start=mid+1;
+                }else{end =mid+1;}
+            }
+        }
+    }
+};
+//solution 3
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& A) {
+        int n = A.size();
+        if (n == 1) return A[0];
+        int start = 0, end = n - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (mid % 2 == 1) mid--;
+            if (A[mid] == A[mid + 1]) start = mid + 2;
+            else end = mid;
+        }
+        return A[start];
     }
 };
