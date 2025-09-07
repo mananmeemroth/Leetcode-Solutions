8e7b325299b24683190f91fc68fe6e803a4d0ec3
Manan Meemroth <divyanshmeemroth@gmail.com>
2025-09-07 22:33:34 +0530

    done for today

---
+++ Daily Problems/1304.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> arr;
+
+        if(n%2!=0){
+            arr.push_back(0);
+        }
+
+        for(int i=1;i<=n/2;i++){
+            arr.push_back(-i);
+            arr.push_back(i);
+        }
+
+        return arr;
+    }
+};
