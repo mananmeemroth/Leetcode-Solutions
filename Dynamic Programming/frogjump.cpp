d3433067991f6b34d0ebf11eeb6032a9255c1aeb
Manan Meemroth <divyanshmeemroth@gmail.com>
2025-10-03 19:12:11 +0530

    new chapter

---
+++ Dynamic Programming/frogjump.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int f(int ind,vector<int>&heights){
+    if(ind==0) return 0;
+    int left = f(ind-1,heights)+abs(heights[ind]-heights[ind-1]);
+    int right = INT_MAX;
+    if(ind>1) right = f(ind-2,heights)+abs(heights[ind]-heights[ind-2]);
+    return min(left,right);
+}
+
+int frogjump(int n,vector<int>&heights){
+    return f(n-1,heights);
+}
+
+// with DP
+
+int f(int ind,vector<int>&heights,vector<int>&dp){
+    if(ind==0) return 0;
+    if(dp[ind]!=-1) return dp[ind];
+    int left = f(ind-1,heights,dp)+abs(heights[ind]-heights[ind-1]);
+    int right = INT_MAX;
+    if(ind>1) right = f(ind-2,heights,dp)+abs(heights[ind]-heights[ind-2]);
+    return dp[ind] = min(left,right);
+}
+
+int frogjump(int n,vector<int>&heights){
+    vector<int> dp(n+1,-1);
+    return f(n-1,heights,dp);
+}
+
+
+
+// with tabulaiton form
