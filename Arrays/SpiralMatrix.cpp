729d1dbb3d7e05a5cd8101458b3389da62df73c4
Manan Meemroth <divyanshmeemroth@gmail.com>
2025-12-25 19:19:08 +0530

    new solutions

---
+++ Arrays/SpiralMatrix.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+class Solution {
+    int dx[4] = {0, 1, 0, -1};
+    int dy[4] = {1, 0, -1, 0};
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = (int) matrix.size();
+        int n = (int) matrix[0].size();
+
+        int visited = -200;
+
+        vector<int> ans(m * n, 0);
+        int direction = 0;
+        int x = 0, y = 0;
+        int cnt = 0;
+
+        while(cnt < m * n) {
+            ans[cnt] = matrix[x][y];
+            matrix[x][y] = visited;
+            cnt++;
+
+            int nx = x + dx[direction];
+            int ny = y + dy[direction];
+
+            if (nx < 0 || nx >= m || ny < 0 || ny >= n || matrix[nx][ny] == visited) {
+                direction = (direction + 1) % 4;
+            }
+
+            x += dx[direction];
+            y += dy[direction];
+        }
+
+        return ans;
+    }
+};
--- Graph/Topo Sort and problems/Toposort.cpp
+++ Graph/Topo Sort and problems/Toposort.cpp
@@ -3,80 +3,27 @@ using namespace std;
 class Solution{
 public:
     void dfs(int node,vector<int> adj[], vector<int>& vis, stack<int>& st){
-        return;
-    }
-    vector<int> topoSort(int V,vector<int> adj[]){
-        
-    }
-};
-
-
-
-
-class Solution {
-public:
-    void dfs(int node, vector<int> adj[], vector<int>& vis, stack<int>& st) {
-        vis[node] = 1;
-        for (auto it : adj[node]) {
-            if (!vis[it]) {
-                dfs(it, adj, vis, st);
+        vis[node]=1;
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                dfs(it,adj,vis,st);
             }
         }
         st.push(node);
     }
-    // Function to perform Topological Sort
-    vector<int> topoSort(int V, vector<int> adj[]) {
-        // Create a visited array to mark visited vertices
-        vector<int> vis(V, 0);
-
-        // Stack to store vertices in finishing order
+    vector<int> topoSort(int V,vector<int> adj[]){
+        vector<int> vis(V,0);
         stack<int> st;
-
-        // Perform DFS from each unvisited vertex
-        for (int i = 0; i < V; i++) {
-            if (!vis[i]) {
-                dfs(i, adj, vis, st);
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                dfs(i,adj,vis,st);
             }
         }
-
-        // Prepare the result array
         vector<int> ans;
-        while (!st.empty()) {
+        while(!st.empty()){
             ans.push_back(st.top());
             st.pop();
         }
-
-        // Return the topological ordering
         return ans;
     }
 };
-
-// Driver code
-int main() {
-    // Number of vertices and edges
-    int V = 6, E = 6;
-
-    // Adjacency list representation of the graph
-    vector<int> adj[V];
-    adj[5].push_back(0);
-    adj[5].push_back(2);
-    adj[4].push_back(0);
-    adj[4].push_back(1);
-    adj[2].push_back(3);
-    adj[3].push_back(1);
-
-    // Create an object of Solution
-    Solution obj;
-
-    // Get the topological order
-    vector<int> res = obj.topoSort(V, adj);
-
-    // Print the result
-    cout << "Topological Sort: ";
-    for (auto it : res) {
-        cout << it << " ";
-    }
-    cout << endl;
-
-    return 0;
-}
