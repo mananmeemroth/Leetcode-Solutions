c4477de853e8012b42c64bdf7e239766598794ff
Manan Meemroth <divyanshmeemroth@gmail.com>
2026-01-21 23:28:17 +0530

    added today's potd solution

--- Binary Tree/TopView.cpp
+++ Binary Tree/TopView.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <queue>
 using namespace std;
 
 struct TreeNode {
@@ -12,65 +13,25 @@ struct TreeNode {
 };
 
 class Solution{
+    public:
     vector<int> topView(TreeNode* root){
-        
-    }
-};
-
-// Class containing the logic for top view
-class Solution {
-public:
-    // Function to return the top view of the binary tree
-    vector<int> topView(TreeNode* root) {
-        // Create a vector to store the final answer
         vector<int> ans;
-
-        // If the tree is empty, return an empty result
-        if (root == NULL) {
-            return ans;
-        }
-
-        // Create a map to store vertical level -> node value (only first encountered)
-        map<int, int> mpp;
-
-        // Create a queue for BFS that stores {node, vertical_level}
-        queue<pair<TreeNode*, int>> q;
-
-        // Push the root node with vertical level 0
-        q.push({root, 0});
-
-        // Start BFS traversal
-        while (!q.empty()) {
-            // Extract the front element of the queue
-            auto it = q.front();
+        if(root==nullptr) return ans;
+        map<int,int> mpp;
+        queue<pair<TreeNode*,int>>q;
+        q.push({root,0});
+        while(!q.empty()){
+            auto it=q.front();
             q.pop();
-
-            // Get the node and its vertical position
             TreeNode* node = it.first;
             int line = it.second;
-
-            // If this vertical position is being visited for the first time, store it
             if (mpp.find(line) == mpp.end()) {
                 mpp[line] = node->val;
             }
-
-            // If there is a left child, push it with vertical level - 1
-            if (node->left != NULL) {
-                q.push({node->left, line - 1});
-            }
-
-            // If there is a right child, push it with vertical level + 1
-            if (node->right != NULL) {
-                q.push({node->right, line + 1});
-            }
+            if(root->left!=nullptr) q.push({root->left,line-1});
+            if(root->right!=nullptr) q.push({root->right,line+1});
         }
-
-        // Traverse the map and store values in the answer vector
-        for (auto it : mpp) {
-            ans.push_back(it.second);
-        }
-
-        // Return the result
+        for(auto it: mpp) ans.push_back(it.second);
         return ans;
     }
 };
